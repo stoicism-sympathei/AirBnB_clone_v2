686e4edb4daf3413ee7a9453add7f00c71615e45
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-19 09:04:16 +0300

    mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,8 @@
 -- Prepares a MySQL server for the hbnb project.
 
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
-USE `hbnb_test_db`;
-
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS hbnb_dev@localhost;
+SET PASSWORD FOR hbnb_dev@localhost = 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO hbnb_dev@localhost;
+GRANT SELECT ON performance_schema . * TO hbnb_dev@localhost;
+FLUSH PRIVILEGES;

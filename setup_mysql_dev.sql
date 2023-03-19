79d48eb9f3f2659049836607c960770e35495dab
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-19 08:21:51 +0300

    mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- Prepares a MySQL server for the hbnb project.
 
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-USE `hbnb_dev_db`;
+USE `hbnb_dev`;
 
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
 	IDENTIFIED BY 'hbnb_dev_pwd';

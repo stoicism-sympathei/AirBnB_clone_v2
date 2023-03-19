36263170587c04aeac0ba8f8a92b79c492829c3a
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-19 09:20:43 +0300

    console module

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Console Module """
+
+import cmd
+from datetime import datetime
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import shlex  # for splitting the line along spaces except in double quotes
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+

3815e623dc0023a3214d5366c2b13062af344160
stoicism-sympatheia <mogesanonymous@gmail.com>
2023-05-24 12:30:59 -0400

    correction

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,13 +6,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello():
     """
-        Flask route at root (http://localhost:5000/).
+        Flask route at /airbnb-onepage/.
         Displays 'Hello HBNB!'.
     """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
+

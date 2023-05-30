7103a214953652b2dd09496d30f5e4bfee7eb612
stoicism-sympatheia <mogesanonymous@gmail.com>
2023-05-30 19:41:02 +0000

    correction

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,6 +15,6 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
